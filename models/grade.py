4bfefc7639fff0ba8f9f66e9107af06cc3e0e02f
cmareau-git <charles_mareau@hotmail.fr>
2025-08-07 11:43:12 +0200

    Initial commit: DB + DB models

---
+++ models/grade.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, SmallInteger, String
+
+from models.base import Base
+import models.boulder
+
+
+class Grade(Base):
+    __tablename__ = "grade"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    value: Mapped[str] = mapped_column(String(3))
+    correspondence: Mapped[int] = mapped_column(SmallInteger)
+
+    # Relationship
+    boulders: Mapped[List["models.boulder.Boulder"]] = relationship(
+        back_populates="grade", foreign_keys="[Boulder.grade_id]"
+    )
+    slash_boulders: Mapped[List["models.boulder.Boulder"]] = relationship(
+        back_populates="slash_grade",
+        foreign_keys="[Boulder.slash_grade_id]",
+    )
+
+    def __repr__(self):
+        return f"<Grade : {self.value}, {self.correspondence}>"
